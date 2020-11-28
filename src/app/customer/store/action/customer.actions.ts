8f9db536446b93ad0a371abbf52af93f05617261
Mansour FALL <fallmansour19@yahoo.fr>
2020-11-29 00:14:11 +0100

    Initial Commit

---
+++ src/app/customer/store/action/customer.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from '@ngrx/store';
+import { Customer } from '../../../models/customer';
+
+export const addCustomer = createAction(
+  '[Customer] Add Customers',
+  (customer: Customer) => ({customer})
+);
+
+
+
+
